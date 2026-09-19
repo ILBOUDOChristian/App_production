b77d918e23ca8edce0cffa80a19b3f554c23428c
ILBOUDOChristian <ilboudochris79@gmail.com>
2026-09-19 18:30:09 +0000

    test: add standard root test/widget_test.dart runner for automated evaluation scripts

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+
+// Importation de l ensemble des tests unitaires et de widgets
+import 'unit/app_unit_test.dart' as unit_tests;
+import 'widget/app_widget_test.dart' as widget_tests;
+
+void main() {
+  group('Unit Tests Suite', unit_tests.main);
+  group('Widget Tests Suite', widget_tests.main);
+}
